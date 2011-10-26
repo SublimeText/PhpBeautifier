06d5afce59052ae34235b2c8e0170f487ba6cf14
Julien Virey <julien.virey@gmail.com>
2011-10-26 09:39:28 +0200

    First commit

---
+++ php_beautifier.py
@@ -0,0 +1,14 @@
+import commands
+import sublime, sublime_plugin
+
+class PhpBeautifierCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        FILE = self.view.file_name()
+        if FILE[-3:] == 'php':
+            PHP_OPTIONS = "-s4 -l 'ArrayNested()' "
+            selection = self.view.sel()[0]
+            replaceRegion = selection if len(selection) > 0 else sublime.Region(0, self.view.size())
+            res = commands.getoutput("php_beautifier " + PHP_OPTIONS +
+            "-f " + self.view.file_name() + " -o -")
+            resu = unicode( res, "utf-8" )
+            self.view.replace(edit, replaceRegion, resu)
