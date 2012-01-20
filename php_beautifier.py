255adbaf24422aa270f4f6e76d40a8fe14bdfb7b
BarbUk <barbarisme@gmail.com>
2012-01-20 09:13:36 +0100

    add fixup for utf8 encoding

--- php_beautifier.py
+++ php_beautifier.py
@@ -1,5 +1,4 @@
-import sublime, sublime_plugin
-import subprocess
+import sublime, sublime_plugin, re, subprocess
 from subprocess import Popen
 from subprocess import PIPE
 
@@ -10,15 +9,17 @@ class PhpBeautifierCommand(sublime_plugin.TextCommand):
             indent = "-s4"
             filters = "ArrayNested() NewLines(before=switch:while:for:foreach:T_CLASS:return:break) Pear(add-header=false)"
             allFile = sublime.Region(0, self.view.size())
-            AllFileText = self.view.substr(allFile)
+            AllFileText = self.view.substr(allFile).encode('utf-8')
             cmd = "php_beautifier"
             p = Popen([cmd, indent, "-l", filters, "-f", "-", "-o", "-"], stdin = PIPE, stdout = PIPE, stderr=PIPE)
             stdout, stderr = p.communicate(AllFileText)
             if len(stderr) == 0:
-                self.view.replace(edit, allFile, stdout)
+                self.view.replace(edit, allFile, self.fixup(stdout))
             else:
-                self.show_error_panel(stderr)
+                self.show_error_panel(self.fixup(stderr))
 
+    # Error panel & fixup from external command
+    # https://github.com/technocoreai/SublimeExternalCommand
     def show_error_panel(self, stderr):
         panel = self.view.window().get_output_panel("php_beautifier_errors")
         panel.set_read_only(False)
@@ -28,3 +29,6 @@ class PhpBeautifierCommand(sublime_plugin.TextCommand):
         panel.set_read_only(True)
         self.view.window().run_command("show_panel", {"panel": "output.php_beautifier_errors"})
         panel.end_edit(edit)
+
+    def fixup(self, string):
+        return re.sub(r'\r\n|\r', '\n', string.decode('utf-8'))
