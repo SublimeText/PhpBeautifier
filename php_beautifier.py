67e618397cff8a1d246da305a8b97f25c5dd71d9
FichteFoll <fichtefoll2@googlemail.com>
2012-12-07 18:45:41 +0100

    Check environment before running
    Closes #5

--- php_beautifier.py
+++ php_beautifier.py
@@ -1,4 +1,5 @@
 import re
+import os
 import subprocess
 
 import sublime
@@ -7,19 +8,34 @@ import sublime_plugin
 
 class PhpBeautifierCommand(sublime_plugin.TextCommand):
     def run(self, edit):
+        # Test environment
+        if self.view.is_scratch():
+            return
+
+        if self.view.is_dirty():
+            return sublime.status_message("Please save the file.")
+
         FILE = self.view.file_name()
-        if FILE[-3:] == 'php':
-            indent = "-s4"
-            filters = "ArrayNested() NewLines(before=switch:while:for:foreach:T_CLASS:return:break) Pear(add-header=false)"
-            allFile = sublime.Region(0, self.view.size())
-            AllFileText = self.view.substr(allFile).encode('utf-8')
-            cmd = "php_beautifier"
-            p = subprocess.Popen([cmd, indent, "-l", filters, "-f", "-", "-o", "-"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            stdout, stderr = p.communicate(AllFileText)
-            if len(stderr) == 0:
-                self.view.replace(edit, allFile, self.fixup(stdout))
-            else:
-                self.show_error_panel(self.fixup(stderr))
+        if not FILE or not os.path.exists(FILE):
+            return self.status("File does not exist.")
+
+        if not FILE[-3:] == 'php':
+            return self.status("File does not have php extension.")
+
+        # Start doing stuff
+        cmd = "php_beautifier"
+        indent = "-s4"
+        filters = "ArrayNested() NewLines(before=switch:while:for:foreach:T_CLASS:return:break) Pear(add-header=false)"
+
+        allFile = sublime.Region(0, self.view.size())
+        AllFileText = self.view.substr(allFile).encode('utf-8')
+
+        p = subprocess.Popen([cmd, indent, "-l", filters, "-f", "-", "-o", "-"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = p.communicate(AllFileText)
+        if len(stderr) == 0:
+            self.view.replace(edit, allFile, self.fixup(stdout))
+        else:
+            self.show_error_panel(self.fixup(stderr))
 
     # Error panel & fixup from external command
     # https://github.com/technocoreai/SublimeExternalCommand
